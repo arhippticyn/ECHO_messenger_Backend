b5364c7c9494ba2bf76ac744a51455e9b0bec123
arhippticyn <kakashihatake6hokagegygg@gmail.com>
2026-03-23 23:12:31 +0200

    Write post endpoint message

--- app/routers/message/message.py
+++ app/routers/message/message.py
@@ -1,9 +1,12 @@
-from fastapi import APIRouter, Depends, HTTPException, status, WebSocket, WebSocketDisconnect
+from fastapi import APIRouter, Depends, HTTPException, status, WebSocket, WebSocketDisconnect, UploadFile, File
 from ...db.session import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from ...models.message.message import Message, MessageType
 from ...services.connection_manager import manager
+from datetime import datetime
+import os
+import shutil
 
 router = APIRouter()
 
@@ -11,16 +14,28 @@ router = APIRouter()
 async def get_messages(chat_id: int, db: AsyncSession = Depends(get_db)):
     return ( await db.execute(select(Message).where(Message.chat_id == chat_id))).scalars().all()
 
+@router.post('/{chat_id}/upload')
+async def upload_file(chat_id: int, file: UploadFile = File(...)):
+    folder = f'uploads/{chat_id}'
+    os.makedirs(folder, exist_ok=True)
+
+    file_path = f'{folder}/{file.filename}'
+
+    with open(file_path, 'wb') as f:
+        shutil.copyfileobj(file.file, f)
+
+    return {'file_url': file_path}
+
 
 @router.websocket('/ws/{chat_id}')
-async def websocket_endpoint(websocket: WebSocket, chat_id: int, db: AsyncSession = Depends(get_db)):
+async def websocket_endpoint(websocket: WebSocket, chat_id: int, db: AsyncSession = Depends(get_db), ):
     await manager.connect(chat_id, websocket)
 
     try:
         while True:
             data = await websocket.receive_json()
 
-            message = Message(chat_id=chat_id, sender_id=data['sender_id'], content=data['content'], type=MessageType.text)
+            message = Message(chat_id=chat_id, sender_id=data['sender_id'], content=data['content'], type=MessageType(data.get('type', 'text')), file_url=data.get('file_url'), created_at=datetime.utcnow())
             db.add(message)
             await db.commit()
             await db.refresh(message)
@@ -34,3 +49,6 @@ async def websocket_endpoint(websocket: WebSocket, chat_id: int, db: AsyncSessio
 
     except WebSocketDisconnect:
         manager.disconnect(chat_id, websocket)
+
+
+
